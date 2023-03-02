9b5e2bc547625dd9ecdf9c22ddb0299d342b0905
Aleksey_Filkin <acid_wind@mail.ru>
2023-03-02 20:03:59 +0700

    NaturalNumbers1toNTask solved

--- NaturalNumbers1toNTask/Program.cs
+++ NaturalNumbers1toNTask/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿//Показать натуральные числа от 1 до N, N задано
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+int firstNum = 1;
+
+string ShowNumbers(int firstNum, int N)
+{
+    if (firstNum <= N) return $"{firstNum} " + ShowNumbers(firstNum+1, N);
+    else return string.Empty;
+}
+
+Console.Write(ShowNumbers(firstNum, N));
