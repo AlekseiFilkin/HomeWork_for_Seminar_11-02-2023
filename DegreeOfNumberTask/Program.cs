fdb7e3a70815fd2dc821ed57694947270d1c8206
Aleksey_Filkin <acid_wind@mail.ru>
2023-03-02 22:29:00 +0700

    DegreeOfNumberTask solved

--- DegreeOfNumberTask/Program.cs
+++ DegreeOfNumberTask/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//Написать программу возведения числа А в целую стень B
+
+Console.Write("Введите число, которе нужно возвести в степень: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите степень: ");
+int deg = int.Parse(Console.ReadLine() ?? "");
+
+int Exponentiation(int num, int deg)
+{
+    if (deg != 0) return Exponentiation(num, deg - 1) * num;
+        else return 1;
+}
+Console.WriteLine($"{num}^{deg} = {Exponentiation(num, deg)}");
