49a2066a9a4a413d6b33b43b9f2a8710cbde1fba
Aleksey_Filkin <acid_wind@mail.ru>
2023-03-02 21:52:37 +0700

    SumDigitsOfNumberTask solved

--- SumDigitsOfNumberTask/Program.cs
+++ SumDigitsOfNumberTask/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿//Найти сумму цифр числа
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+int SumDigits(int num)
+{
+    if (num != 0)
+    {
+        return num % 10 + SumDigits(num / 10);
+    }
+    else
+    {
+        return 0;
+    }
+}
+Console.WriteLine($"Сумма цифр числа {num} равна {SumDigits(num)}");
