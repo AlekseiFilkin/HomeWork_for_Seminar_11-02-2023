a8b5fc931adaa9f8fcf19b43e5b47488eca11e14
Aleksey_Filkin <acid_wind@mail.ru>
2023-03-02 20:13:49 +0700

    NaturalNumbersNto1Task solved

--- NaturalNumbersNto1Task/Program.cs
+++ NaturalNumbersNto1Task/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿//Показать натуральные числа от N до 1, N задано
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+int lastNum = 1;
+
+string ShowNumbers(int N, int lastNum)
+{
+    if (N >= lastNum) return $"{N} " + ShowNumbers(N-1, lastNum);
+    else return string.Empty;
+}
+
+Console.Write(ShowNumbers(N, lastNum));
