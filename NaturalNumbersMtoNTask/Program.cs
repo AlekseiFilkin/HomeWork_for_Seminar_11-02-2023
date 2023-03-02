6dcd59912015eefd0c441a0524a130b73b75eaa3
Aleksey_Filkin <acid_wind@mail.ru>
2023-03-02 21:38:33 +0700

    NaturalNumbersMtoNTask solved

--- NaturalNumbersMtoNTask/Program.cs
+++ NaturalNumbersMtoNTask/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//Показать натуральные числа от M до N, N и M заданы
+Console.Write("Введите M: ");
+int M = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+string ShowNumbers(int M, int N)
+{
+    if (M <= N) return $"{M} " + ShowNumbers(M + 1, N);
+    else return String.Empty;
+}
+
+Console.Write(ShowNumbers(M, N));
