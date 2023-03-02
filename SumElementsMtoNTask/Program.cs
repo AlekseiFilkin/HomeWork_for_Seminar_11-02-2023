b1f280b37517a1ab5fa517a59772dd16aa580fab
Aleksey_Filkin <acid_wind@mail.ru>
2023-03-02 21:46:01 +0700

    SumElementsMtoNTask solved

--- SumElementsMtoNTask/Program.cs
+++ SumElementsMtoNTask/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//Найти сумму элементов от M до N, N и M заданы
+Console.Write("Введите M: ");
+int M = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+int SumElements(int M, int N)
+{
+    if (M < N) return M + SumElements(M + 1, N);
+    else return 0;
+}
+
+Console.Write($"Суммф всех элементов от {M} до {N} равна {SumElements(M, N)}");
